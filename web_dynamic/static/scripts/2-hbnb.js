178d84b91573b0b4f63f45b3f2f1bb8602f0146e
chimaskyy <chiamakahj@gmail.com>
2024-03-04 10:31:14 +0100

    2 fixed

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -2,19 +2,22 @@ $(document).ready(function () {
   const selectedAmenities = [];
 
   // listen for changes in the input checkbox
-  $('input[type="checkbox"]').change(function () {
-    const amenityId = $(this).data("name");
+  $('input:checkbox').change(function () {
+    let amenityId = $(this).data("name");
 
     if ($(this).is(":checked")) {
       selectedAmenities.push(amenityId);
     } else {
+      // Find the index of the amenity name in the selectedAmenities array
       const index = selectedAmenities.indexOf(amenityId);
+      // Check if the amenity name already exists in the array
       if (index !== -1) {
         selectedAmenities.splice(index, 1);
       }
     }
-    const amenitiesText = selectedAmenities.join(", ");
-    $("div.amenities h4").text(amenitiesText);
+    $("DIV.amenities h4").empty();
+    let amenitiesName = selectedAmenities.join(", ")
+    $("DIV.amenities h4").text(amenitiesName);
   });
   
   $.get("http://localhost:5001/api/v1/status/", function (data) {
